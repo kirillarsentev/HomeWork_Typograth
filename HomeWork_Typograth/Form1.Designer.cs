9cff19b634995d60df96fc1100a6f6fe2df64833
Kirill_arsentev <arsen@DESKTOP-4AJNJ1I>
2023-05-11 17:29:29 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_Typograth/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Typograth_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.to_do_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(12, 12);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(1155, 588);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.Text = "";
+            // 
+            // to_do_btn
+            // 
+            this.to_do_btn.Location = new System.Drawing.Point(990, 645);
+            this.to_do_btn.Name = "to_do_btn";
+            this.to_do_btn.Size = new System.Drawing.Size(177, 37);
+            this.to_do_btn.TabIndex = 1;
+            this.to_do_btn.Text = "Редактировать";
+            this.to_do_btn.UseVisualStyleBackColor = true;
+            this.to_do_btn.Click += new System.EventHandler(this.buttonToDo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1179, 757);
+            this.Controls.Add(this.to_do_btn);
+            this.Controls.Add(this.TextBox);
+            this.Name = "Form1";
+            this.Text = "Типограф";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button to_do_btn;
+    }
+}
+
---
+++ HomeWork_Typograth/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Typograth_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void buttonToDo_Click(object sender, EventArgs e)
+        {
+            TextBox.ReadOnly = false;
+            TextBox.Text = space(TextBox.Text);
+            TextBox.Text = maternoye_slovo(TextBox.Text);
+            TextBox.Text = dots(TextBox.Text);
+            TextBox.Text = plus_minus(TextBox.Text);
+            TextBox.Text = defis(TextBox.Text);
+            TextBox.Text = best_teachers(TextBox.Text);
+            
+        }
+        public string space(string str)
+        {
+            str.Trim();
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            str = string.Join(" ", words);
+            return str;
+        }
+        
+        public string dots(string str)
+        {
+            str = str.Replace("...", "\u2026");
+            return str;
+        }
+        public string best_teachers(string str)
+        {
+                    while(str.Contains("Дина Сергеевна"))
+                    {
+                        str = str.Replace("Дина Сергеевна", "Лучший препод");
+                    }
+        
+            return str;
+
+        }
+
+        public string plus_minus(string str)
+        {
+            str = str.Replace("+-", "±");
+            return str;
+        }
+
+        public string defis(string str)
+        {
+            str = str.Replace(" — ", "—");
+            return str;
+        }
+
+        public string maternoye_slovo(string str)
+        {
+            string [] words = str.Split(' ');
+            string[] word = new string[5] {"мат","сквернословие","брань","ругательство","ругань"};
+            for(int i = 0; i < words.Length; i++)
+            {
+                for(int j = 0; j < word.Length; j++)
+                {
+                    if(words[i] == word[j])
+                    {
+                        str = str.Replace(word[j], "***");
+                    }
+                }
+            }
+            return str;
+           
+        }
+    }
+}
